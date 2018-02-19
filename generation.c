3d9ea84b8d9bf74061b053e488778831ca875965
remi kajak <l2info041@ic2s122-11.univ-lemans.fr>
2018-02-19 14:33:36 +0100

    Finalisation de la fonction lecture_fic et dernier changement du format du fichier contenant les données des puzzles

--- generation.c
+++ generation.c
@@ -3,38 +3,43 @@
 #include <string.h>
 
 #define N 10
-int mat[N][N];
-
-void affectation_nb_matrice(int mat[N][N],int ligne) {
-	
-}
+#define M 5
+int mat[N][M];
 
 void lecture_fic(char *nom_fic, int niveau) { /* Lecture du fichier contenant les puzzles prédéfinis */
 	FILE * fic_gen;
 	char cle[4] = "PHVF", carac;
-	int i, num_ligne, num_puz, nb_case;
+	int ligne, colonne, num_puz, nb_case;
 	
 	fic_gen = fopen(nom_fic,"r");
 	fscanf(fic_gen,"%c %i",&carac,&num_puz);
 	
-	while(!feof(fic_gen)) {
-		if(carac == cle[0] && num_puz == niveau) {
+	while(!feof(fic_gen)) { // Tant que la fin du fichier n'a pas été atteinte
+		if(carac == cle[0] && num_puz == niveau) { // Si on trouve le bon niveau de puzzle
 			printf("%c %i\n",carac,num_puz);
-			for(i = 1; i <= 2; i++) {
-				num_ligne = 0;
+			for(int i = 1; i <= 2; i++) { // Lecture successive des clés
+				ligne = 0;
+				colonne = 0;
 				
-				while(carac != cle[i]) {
+				while(carac != cle[i]) { // Tant que l'on n'a pas trouvé la clé de départ
 					fscanf(fic_gen,"%c",&carac);
 				}
 				printf("%c",carac);
 				fscanf(fic_gen,"%c",&carac);
-				while(carac != cle[i+1]) {
+				
+				while(carac != cle[i+1]) { // Tant que l'on n'a pas trouvé la clé de fin
 					printf("%c",carac);
 					if(carac == ' ' || carac == '\n') {
 						fscanf(fic_gen,"%i",&nb_case);
 						printf("%i",nb_case);
-						printf("%i\n",num_ligne);
-						if(carac == '\n') num_ligne++;
+						mat[ligne][colonne] = nb_case;
+						colonne++;
+					}
+					else if(carac == '+') {
+						ligne++;
+						printf(" %i",ligne-1);
+						printf(" %i",colonne);
+						colonne = 0;
 					}
 					fscanf(fic_gen,"%c",&carac);
 				}
