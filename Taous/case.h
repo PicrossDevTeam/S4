07bc345115d13567dc0c7a63276d4d52776cd86c
L2info041 <remi.kajak.etu@univ-lemans.fr>
2018-03-21 23:09:55 +0100

    Mise à jour du dépôt local

--- Taous/case.h
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<stdlib.h>
-
-
-
-typedef enum couleurs{blanc, noir, croix}t_couleurs;
-typedef struct Case{t_couleurs etat;}t_Case;
-double * creerMatrice(int n);
-void afficherMatrice(int n, double * A);
