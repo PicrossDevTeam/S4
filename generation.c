3d6a017c6d67b969ef43b110b2be7fdaf9a108b5
remi kajak <l2info041@ic2s114-14.univ-lemans.fr>
2018-03-27 16:30:27 +0200

    Début du codage de la fonction de génération de la matrice solution

--- generation.c
+++ generation.c
@@ -3,7 +3,7 @@
 * \author KAJAK Rémi
 * \version 1.0
 * \date 19/03/2018
-* \brief Fichier ayant servi à l'élaboration de la fonction de lecture du fichier texte qui contient les nombres générateurs de la matrice principale.
+* \brief Fichier ayant servi à l'élaboration de la fonction de lecture du fichier texte qui contient les nombres générateurs de la matrice principale et la fonction de génération de la matrice solution.
 */
 #include <stdio.h>
 #include <stdlib.h>
@@ -13,7 +13,7 @@
 * \def N
 * \brief Nombre de lignes de la matrice test
 */
-#define N 10
+#define N 5
 
 /**
 * \fn lecture_fic(char *nom, int niveau)
@@ -32,7 +32,6 @@ void lecture_fic(char *nom_fic, int niveau, int H[N][N], int V[N][N]) { /* Lectu
 
 	while(!feof(fic_gen)) { // Tant que la fin du fichier n'a pas été atteinte
 		if(carac == cle[0] && num_puz == niveau) { // Si on trouve le bon niveau de puzzle
-			printf("%c %i\n",carac,num_puz);
 			for(i = 1; i <= 2; i++) { // Lecture successive des clés
 				ligne = 0;
 				colonne = 0;
@@ -40,44 +39,63 @@ void lecture_fic(char *nom_fic, int niveau, int H[N][N], int V[N][N]) { /* Lectu
 				while(carac != cle[i]) { // Tant que l'on n'a pas trouvé la clé de départ
 					fscanf(fic_gen,"%c",&carac);
 				}
-				printf("%c",carac);
 				fscanf(fic_gen,"%c",&carac);
 
 				while(carac != cle[i+1]) { // Tant que l'on n'a pas trouvé la clé de fin
-					printf("%c",carac);
 					if(carac == ' ' || carac == '\n') {
 						fscanf(fic_gen,"%i",&nb_case);
-						printf("%i",nb_case);
 						if(cle[i] == 'H') H[colonne][ligne] = nb_case;
 						else if(cle[i] == 'V') V[ligne][colonne] = nb_case;
 						colonne++;
 					}
 					else if(carac == '+') {
 						ligne++;
-						printf(" %i",ligne-1);
-						printf(" %i",colonne);
 						colonne = 0;
 					}
 					fscanf(fic_gen,"%c",&carac);
 				}
-				printf("\n\n");
 			}
 		}
 		fscanf(fic_gen,"%c %i",&carac,&num_puz);
 	}
 	fclose(fic_gen);
-	printf("Sortie du fichier\n");
 }
 
-void init_matrices(int H[N][N], int V[N][N]) {
-	int i, j;
-<<<<<<< HEAD
+void init_compteurs_periph(int cptH[N], int cptV[N]) {
+	int i;
 	
-=======
+	for(i = 0; i < N; i++) {
+		cptH[i] = 0;
+		cptV[i] = 0;
+	}
+}
 
->>>>>>> f4e57341a03ad6a47a8736aabd4e59cd69ffc46f
+void gen_solution(int S[N][N], int H[N][N], int V[N][N]) {
+	int nbH[N], nbV[N], i, j;
+	
+	// Dans un premier temps, on dénombre les chiffres présents dans chacune des lignes des matrices périphériques
+	init_compteurs_periph(nbH,nbV);
 	for(i = 0; i < N; i++) {
 		for(j = 0; j < N; j++) {
+			if(H[j][i] > 0) nbH[i] = nbH[i] + 1;
+			if(V[i][j] > 0) nbV[i] = nbV[i] + 1;
+		}
+	}
+	
+	// Ensuite, 
+	for(i = 0; i < N; i++) {
+		for(j = 0; j < N; j++) {
+			
+		}
+	}
+}
+
+void init_matrices(int S[N][N], int H[N][N], int V[N][N]) {
+	int i, j;
+	
+	for(i = 0; i < N; i++) {
+		for(j = 0; j < N; j++) {
+			S[i][j] = 0;
 			H[i][j] = 0;
 			V[i][j] = 0;
 		}
@@ -86,47 +104,42 @@ void init_matrices(int H[N][N], int V[N][N]) {
 
 void afficher_matrice(int mat[N][N], char cle) {
 	int i, j;
-<<<<<<< HEAD
 	
-=======
-
->>>>>>> f4e57341a03ad6a47a8736aabd4e59cd69ffc46f
+	printf("\n");
 	if(cle == 'H') {
 		printf("Matrice périphérique horizontale :\n");
 		for(i = 0; i < N/2; i++) {
 			for(j = 0; j < N; j++) printf("%i ",mat[i][j]);
+			printf("\n");
 		}
-<<<<<<< HEAD
-		printf("\n\n");
-=======
-		printf("\n");
->>>>>>> f4e57341a03ad6a47a8736aabd4e59cd69ffc46f
+		
 	}
 	else if(cle == 'V') {
 		printf("Matrice périphérique verticale :\n");
 		for(i = 0; i < N; i++) {
 			for(j = 0; j < N/2; j++) printf("%i ",mat[i][j]);
+			printf("\n");
 		}
-		printf("\n\n");
-<<<<<<< HEAD
 	}
-=======
-
+	else {
+		printf("Matrice solution :\n");
+		for(i = 0; i < N; i++) {
+			for(j = 0; j < N; j++) printf("%i ",mat[i][j]);
+			printf("\n");
+		}
 	}
-	printf("\n\n");
->>>>>>> f4e57341a03ad6a47a8736aabd4e59cd69ffc46f
 }
 
 int main() {
 	char *saisie = "nombres_puzzle.txt";
-	int matH[N][N], matV[N][N];
-<<<<<<< HEAD
+	int matH[N][N], matV[N][N], soluce[N][N];
 	
-=======
-
->>>>>>> f4e57341a03ad6a47a8736aabd4e59cd69ffc46f
-	init_matrices(matH,matV);
-	lecture_fic(saisie,2,matH,matV);
+	init_matrices(soluce,matH,matV);
+	lecture_fic(saisie,1,matH,matV);
 	afficher_matrice(matH,'H');
 	afficher_matrice(matV,'V');
+	
+	gen_solution(soluce,matH,matV);
+	afficher_matrice(soluce,'S');
+	printf("\n");
 }
