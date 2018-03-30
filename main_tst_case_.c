17a3208e77f71da61e13349d382101d212954024
erick022 <36163137+erick022@users.noreply.github.com>
2018-03-30 12:00:28 +0200

    main_test_case_.c mise à jour

--- main_tst_case_.c
+++ main_tst_case_.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "case.h"
 
 int caz_blanche();
@@ -7,9 +8,9 @@ int modif_caz();
 
 int main()
 {
-	t_caz caz;
+	t_caz caz = NULL;
 	int vide;
-	int somme_coup = 0;
+	int comp_coup = 0;
 	int puzzle_resolu = 0;
 	int croix1 = 1;
 	int croix2 = 0;
@@ -30,27 +31,26 @@ int main()
 		printf("\n");
 	}
 
-	
 	do{
-		vide = caz_blanche(caz); //cette instruction reenvoie un WARNING en essayant de debugger " gcc main.c -g -Wall -Wextra " je suis bloqué
+		vide = caz_blanche(caz);
 		printf("\njouer\n");
 		scanf("%d",&puzzle_resolu);
 
 		if(vide){
 			modif_caz(caz,croix1);
+			comp_coup++;
 			return M[i][j] = 1;
-			somme_coup += 1;
 		}
 		else{
 			modif_caz(caz,croix2);
+			comp_coup++;
 			return M[i][j] = 0;
-			somme_coup +=1;
 		}
 	}
 	while(!puzzle_resolu);
 	if(caz_noire(caz) == 0)
-		printf("Felicitation vous avez resolu le puzzle avec succès!!\n");
-
+		printf("Felicitation vous avez resolu le puzzle avec succès!!\nEn %d coup jouer", comp_coup);
+		printf("\n");
 
 	return 0;
 }
