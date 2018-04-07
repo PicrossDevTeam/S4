e2237c2ac245b290b306bf34aa5ecde90411bada
nouveliere-benjamin <36163252+nouveliere-benjamin@users.noreply.github.com>
2018-04-07 15:30:07 +0200

    Manque réduction fonction avec booléen

--- matrice.c
+++ matrice.c
@@ -2,99 +2,73 @@
 #include <stdlib.h>
 #include <math.h>
 
-/* Taille de base de la matrice */
-const int N = 5;
+typedef enum couleurs {Blanche, Noire, Croix} t_couleurs;
+typedef enum difficulte {facile, normal, difficile, expert} t_difficulte;
 
-/* Taille après le choix de la difficulté */
-int T;
-
-/* Taille des matrices périphérique pour éviter un parcours trop long */
-int P;
-
-void creer_mat_ori(int mat_ori[T][T]){
+t_couleurs * init_matrice_prin(int dim_mat){
+	t_couleurs * mat = malloc(dim_mat*dim_mat*sizeof(t_couleurs));
 	int i,j;
 
-	for(i=0;i<T;i++){
-		for(j=0;j<T;j++){
-			mat_ori[i][j]=0;
+	for(i=0 ; i < dim_mat ; i++) {
+		for(j=0 ; j < dim_mat ; j++) {
+			mat[dim_mat*i+j] = Blanche;
 		}
 	}
+	return mat;
 }
 
-void creer_peri_haut(int mat_ori[T][T]){
+int * init_matrice_peri(int dim_mat){
+	int * mat = malloc(dim_mat*dim_mat*sizeof(int));
 	int i,j;
 
-	for(i=0;i<P;i++){
-		for(j=0;j<T;j++){
-			mat_ori[i][j]=0;
+	for(i=0 ; i < dim_mat ; i++) {
+		for(j=0 ; j < dim_mat ; j++) {
+			mat[dim_mat*i+j] = 0;
 		}
 	}
+	return mat;
 }
 
-void creer_peri_gauche(int mat_ori[T][T]){
+void detruire_matrice_peri(int *mat) {
+	free(mat);
+}
+
+void detruire_matrice_prin(t_couleurs *mat){
+	free(mat);
+}
+
+void afficher_haut(int *mat_hori, int dim_mat){
 	int i,j;
 
-	for(i=0;i<T;i++){
-		for(j=0;j<P;j++){
-			mat_ori[i][j]=0;
+	for(i = 0; i < dim_mat; i++) {
+		for(j = 0; j < dim_mat; j++) {
+			printf("  ");
 		}
-	}
-}
-
-void aff_mat_ori(int mat_ori[T][T]){
-	int i,j;
-
-	for(i=0;i<T;i++){
-		for(j=0;j<T;j++){
-			printf("%i ", mat_ori[i][j]);
-		}
-		printf("\n");
-	}
-}
-
-void aff_peri_haut(int mat_ori[T][T]){
-	int i,j;
-
-	for(i=0;i<P;i++){
-		for(j=0;j<T;j++){
-			printf("%i ", mat_ori[i][j]);
+		printf(" ");
+		for(j = 0; j < dim_mat; j++){
+			printf("%i ",mat_hori[dim_mat*i+j]);
 		}
 		printf("\n");
 	}
-}
-
-void aff_peri_gauche(int mat_ori[T][T]){
-	int i,j;
-
-	for(i=0;i<T;i++){
-		for(j=0;j<P;j++){
-			printf("%i ", mat_ori[i][j]);
-		}
-		printf("\n");
-	}
-}
-
-/*
-int main(){
-	
-	T = N * 5;
-
-	P = N * 2;
-
-	int mat_ori[T][T];
-	int mat_peri_haut[P][T];
-	int mat_peri_gauche[T][P];
-
-	creer_mat_ori(mat_ori);
-	aff_mat_ori(mat_ori);
-
 	printf("\n");
-	creer_peri_haut(mat_peri_haut);
-	aff_peri_haut(mat_peri_haut);
-
-	printf("\n");
-	creer_peri_gauche(mat_peri_gauche);
-	aff_peri_gauche(mat_peri_gauche);
 }
 
-*/
+void afficher_bas(int *mat_verti, t_couleurs *mat_prin, int dim_mat){
+	int i,j;
+
+	for(i = 0; i < dim_mat; i++) {
+		for(j = 0; j < dim_mat; j++) {
+			printf("%i ", mat_verti[dim_mat*i+j]);
+		}
+		printf(" ");
+		for(j = 0; j < dim_mat; j++){
+			printf("%i ", mat_prin[dim_mat*i+j]);
+		}
+		printf("\n");
+	}
+}
+
+void affichage_jeu(int *mat_hori, int *mat_verti, t_couleurs *mat_prin, int dim_mat){
+	afficher_haut(mat_verti, dim_mat);
+	afficher_bas(mat_verti, mat_prin, dim_mat);
+}
