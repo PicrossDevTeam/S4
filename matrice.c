280b336972f34be2d02b35e7517158d7dbd46897
L2info041 <36078496+L2info041@users.noreply.github.com>
2018-04-07 15:40:22 +0200

    Correction de matrice.c
    Concaténation des fonctions similaires en une seule selon le rôle qu'elles remplissent.

--- matrice.c
+++ matrice.c
@@ -1,29 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <math.h>
 
 typedef enum couleurs {Blanche, Noire, Croix} t_couleurs;
 typedef enum difficulte {facile, normal, difficile, expert} t_difficulte;
 
-t_couleurs * init_matrice_prin(int dim_mat){
-	t_couleurs * mat = malloc(dim_mat*dim_mat*sizeof(t_couleurs));
-	int i,j;
+t_couleurs * init_matrice(int dim_mat, _Bool entier){
+	int i, j;
+	
+	if(entier == false) {
+		t_couleurs * mat = malloc(dim_mat*dim_mat*sizeof(t_couleurs));
 
-	for(i=0 ; i < dim_mat ; i++) {
-		for(j=0 ; j < dim_mat ; j++) {
-			mat[dim_mat*i+j] = Blanche;
+		for(i=0 ; i < dim_mat ; i++) {
+			for(j=0 ; j < dim_mat ; j++) {
+				mat[dim_mat*i+j] = Blanche;
+			}
 		}
 	}
-	return mat;
-}
+	else {
+		int * mat = malloc(dim_mat*dim_mat*sizeof(int));
 
-int * init_matrice_peri(int dim_mat){
-	int * mat = malloc(dim_mat*dim_mat*sizeof(int));
-	int i,j;
-
-	for(i=0 ; i < dim_mat ; i++) {
-		for(j=0 ; j < dim_mat ; j++) {
-			mat[dim_mat*i+j] = 0;
+		for(i=0 ; i < dim_mat ; i++) {
+			for(j=0 ; j < dim_mat ; j++) {
+				mat[dim_mat*i+j] = 0;
+			}
 		}
 	}
 	return mat;
@@ -37,8 +38,8 @@ void detruire_matrice_prin(t_couleurs *mat){
 	free(mat);
 }
 
-void afficher_haut(int *mat_hori, int dim_mat){
-	int i,j;
+void affichage_jeu(int *mat_hori, int *mat_verti, t_couleurs *mat_prin, int dim_mat){
+	int i, j;
 
 	for(i = 0; i < dim_mat; i++) {
 		for(j = 0; j < dim_mat; j++) {
@@ -51,11 +52,7 @@ void afficher_haut(int *mat_hori, int dim_mat){
 		printf("\n");
 	}
 	printf("\n");
-}
-
-void afficher_bas(int *mat_verti, t_couleurs *mat_prin, int dim_mat){
-	int i,j;
-
+	
 	for(i = 0; i < dim_mat; i++) {
 		for(j = 0; j < dim_mat; j++) {
 			printf("%i ", mat_verti[dim_mat*i+j]);
@@ -66,9 +63,4 @@ void afficher_bas(int *mat_verti, t_couleurs *mat_prin, int dim_mat){
 		}
 		printf("\n");
 	}
-}
-
-void affichage_jeu(int *mat_hori, int *mat_verti, t_couleurs *mat_prin, int dim_mat){
-	afficher_haut(mat_verti, dim_mat);
-	afficher_bas(mat_verti, mat_prin, dim_mat);
 }
